51a29eb8e414636dbf96c1b41de057a1d3cf283f
exquisiteYe <xyee8023@163.com>
2020-09-09 11:45:09 +0800

    一些修改的文件

--- src/main/java/cn/edu/bupt/admin/adminexception/AdminAccessPermissionException.java
+++ src/main/java/cn/edu/bupt/admin/adminexception/AdminAccessPermissionException.java
@@ -1,4 +1,8 @@
 package cn.edu.bupt.admin.adminexception;
 
-public class AdminAccessPermissionException {
+
+public class AdminAccessPermissionException extends Exception {
+    AdminAccessPermissionException(){super();}
+    public AdminAccessPermissionException(String message){super(message);}
+
 }
--- src/main/java/cn/edu/bupt/admin/handler/AdminController.java
+++ src/main/java/cn/edu/bupt/admin/handler/AdminController.java
@@ -1,4 +1,28 @@
 package cn.edu.bupt.admin.handler;
 
+import cn.edu.bupt.admin.adminexception.AdminAccessPermissionException;
+import cn.edu.bupt.admin.model.Admin;
+import cn.edu.bupt.admin.service.AdminService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.portlet.ModelAndView;
+import org.springframework.web.portlet.bind.annotation.ResourceMapping;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
 public class AdminController {
+    @Autowired
+    AdminService adminService;
+
+    @ResourceMapping("login.do")
+    public ModelAndView login(Admin admin, HttpServletRequest request) throws AdminAccessPermissionException {
+        ModelAndView modelAndView = new ModelAndView();
+        adminService.login(admin);
+        // 下面两步干啥的？
+        modelAndView.addObject("admin", admin);
+        request.getSession().setAttribute("admin", admin);
+        modelAndView.setViewName("/redirect: jsps/admin/main.jsp");
+        return modelAndView;
+    }
 }
